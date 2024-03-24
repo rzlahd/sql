dda2a5935d231c89cf532f87af8a58b1799a6488
izal <58200011+rzlahd@users.noreply.github.com>
2024-03-24 15:20:09 +0700

    Update table_join.sql

--- table_join.sql
+++ table_join.sql
@@ -1,7 +1,11 @@
-SELECT mandi.id_mandi AS idmandi, 
+SELECT 
+  -- alias mana collom
+  mandi.id_mandi AS idmandi, 
   data_cat.kc_nama AS namacat, 
   mandi.waktu_dimandikan AS dimadikan, 
   mandi.waktu_selanjutnya AS selanjutnya 
 FROM mandi 
+-- join id_mandi
 JOIN data_cat ON mandi.id_mandi=data_cat.id_kc 
+--  cari dimana id mandi
 WHERE mandi.id_mandi = 'A1'; 
