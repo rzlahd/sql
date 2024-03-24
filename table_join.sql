4f7baa3d55b717b2148af53cbc9ab67db80d3817
izal <58200011+rzlahd@users.noreply.github.com>
2024-03-24 15:18:26 +0700

    Update table_join.sql

--- table_join.sql
+++ table_join.sql
@@ -1,1 +1,7 @@
-SELECT mandi.id_mandi AS idmandi, data_cat.kc_nama AS namacat, mandi.waktu_dimandikan AS dimadikan, mandi.waktu_selanjutnya AS selanjutnya FROM mandi JOIN data_cat ON mandi.id_mandi=data_cat.id_kc WHERE mandi.id_mandi = 'A1'; 
+SELECT mandi.id_mandi AS idmandi, 
+  data_cat.kc_nama AS namacat, 
+  mandi.waktu_dimandikan AS dimadikan, 
+  mandi.waktu_selanjutnya AS selanjutnya 
+FROM mandi 
+JOIN data_cat ON mandi.id_mandi=data_cat.id_kc 
+WHERE mandi.id_mandi = 'A1'; 
